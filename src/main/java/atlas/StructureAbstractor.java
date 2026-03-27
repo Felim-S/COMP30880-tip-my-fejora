893c8cd1e7a54512cbba40354a31156c6a232c4f
7joey <joeymcentee321@gmail.com>
2026-03-27 16:37:11 +0000

    Started testing StructureAbstractor

--- src/main/java/atlas/StructureAbstractor.java
+++ src/main/java/atlas/StructureAbstractor.java
@@ -15,11 +15,11 @@ public class StructureAbstractor {
         Structure abstraction = new Structure();
 
         for(Element element : structure.getElements()) {
-            if(element instanceof Symbol) {
+            if(element instanceof atlas.Symbol) {
                 abstraction.addElement(new Predicate(((Predicate) element).getValue()));
             }
 
-            else if(element instanceof Symbol) {
+            else if(element instanceof atlas.Symbol) {
                 String value =  ((Symbol) element).getValue();
 
                 if(!map.containsKey(value)) {
@@ -30,7 +30,7 @@ public class StructureAbstractor {
                 abstraction.addElement(new Symbol(String.valueOf(number)));
             }
 
-            else if(element instanceof Structure) {
+            else if(element instanceof atlas.Structure) {
                 Structure child =  (Structure) element;
                 Structure abstractChild = abstractStructure(child, map, counter);
                 abstraction.addElement(abstractChild);
---
+++ src/test/java/atlas/StructureAbstractorTest.java
@@ -0,0 +1,11 @@
+package atlas;
+
+import org.junit.Test;
+
+import static atlas.StructureAbstractor.generateAbstraction;
+import static org.junit.Assert.*;
+
+public class StructureAbstractorTest {
+    @Test
+    public void testBasicAbstraction() {
+}
