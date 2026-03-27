d2b04a4dee74576040cc34f3b00f903306570d21
7joey <joeymcentee321@gmail.com>
2026-03-27 16:05:05 +0000

    Implemented logic

--- src/main/java/atlas/StructureAbstractor.java
+++ src/main/java/atlas/StructureAbstractor.java
@@ -1,16 +1,43 @@
 package atlas;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class StructureAbstractor {
     public static Structure generateAbstraction(Structure structure) {
-        // TODO - User Story #3
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        int[] counter = {0};
+        return abstractStructure(structure, map, counter);
     }
 
     private static Structure abstractStructure(Structure structure, Map<String, Integer> map, int[] counter) {
 
+        Structure abstraction = new Structure();
 
-        return null;
+        for(Element element : structure.getElements()) {
+            if(element instanceof Symbol) {
+                abstraction.addElement(new Predicate(((Predicate) element).getValue()));
+            }
+
+            else if(element instanceof Symbol) {
+                String value =  ((Symbol) element).getValue();
+
+                if(!map.containsKey(value)) {
+                    map.put(value, counter[0]++);
+                }
+
+                int number = map.get(value);
+                abstraction.addElement(new Symbol(String.valueOf(number)));
+            }
+
+            else if(element instanceof Structure) {
+                Structure child =  (Structure) element;
+                Structure abstractChild = abstractStructure(child, map, counter);
+                abstraction.addElement(abstractChild);
+            }
+        }
+
+
+        return abstraction;
     }
 }
