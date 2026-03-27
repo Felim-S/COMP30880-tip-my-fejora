b081681cc728eca19c590aefc0d149085b25eb0b
7joey <joeymcentee321@gmail.com>
2026-03-27 15:55:34 +0000

    Added class structure user story #3

--- src/main/java/atlas/StructureAbstractor.java
+++ src/main/java/atlas/StructureAbstractor.java
@@ -1,8 +1,16 @@
 package atlas;
 
+import java.util.Map;
+
 public class StructureAbstractor {
     public static Structure generateAbstraction(Structure structure) {
         // TODO - User Story #3
+        return null;
+    }
+
+    private static Structure abstractStructure(Structure structure, Map<String, Integer> map, int[] counter) {
+
+
         return null;
     }
 }
