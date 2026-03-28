fc47bf4c1e1ee970f28808cbe344eb13f0de8d08
7joey <joeymcentee321@gmail.com>
2026-03-28 17:17:26 +0000

    structure testing

--- src/test/java/atlas/StructureAbstractorTest.java
+++ src/test/java/atlas/StructureAbstractorTest.java
@@ -26,4 +26,28 @@ public class StructureAbstractorTest {
         //scientist and laboratory should both be replaced with respective numbers
     }
 
+    @Test
+    public void testSymbolsReplacedWithNumbers2(){
+        Structure result = parseAndAbstract("(serve priest (some congregation (that (perform worship))))");
+
+        List<Element> elements = result.getElements();
+        assertEquals("serve", elements.get(0).toString());
+        assertEquals("0", elements.get(1).toString());
+
+        Structure someStructure = (Structure) elements.get(2);
+        List<Element> someElements = someStructure.getElements();
+        assertEquals("some",  someElements.get(0).toString());
+        assertEquals("1",   someElements.get(1).toString());
+
+        Structure thatStructure = (Structure) someElements.get(2);
+        List<Element> thatElements = thatStructure.getElements();
+        assertEquals("that", thatElements.get(0).toString());
+
+        Structure performStructure = (Structure) thatElements.get(1);
+        List<Element> performElements = performStructure.getElements();
+        assertEquals("perform", performElements.get(0).toString());
+        assertEquals("2",   performElements.get(1).toString());
+
+    }
+
 }
