106fdcd18c08600f8077c0231b8bcc79c50f4ba1
ciaranibhradaigh <ciara.nibhradaigh@ucdconnect.ie>
2026-03-28 22:51:58 +0000

    User Story #2

--- src/main/java/atlas/Structure.java
+++ src/main/java/atlas/Structure.java
@@ -8,12 +8,57 @@ public class Structure extends Element {
     private final List<Element> elements = new ArrayList<>();
 
     @Override
-    public boolean isStructure() { return true; }
+    public boolean isStructure() {
+        return true;
+    }
+
     @Override
     public String toString() {
-        // TODO - User Story #2
-        return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append("(");
+
+        for (int i = 0; i < elements.size(); i++) {
+            sb.append(elements.get(i).toString());
+
+            if (i < elements.size() - 1) {
+                sb.append(" ");
+            }
+        }
+        sb.append(")");
+        return sb.toString();
     }
+
+    public String toIndentedString() {
+        return toIndentedString(0);
+    }
+
+    private String toIndentedString(int indent) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < indent; i++) {
+            sb.append("\t");
+        }
+
+        sb.append("(");
+
+        for (int i = 0; i < elements.size(); i++) {
+            Element element = elements.get(i);
+            if (element instanceof Structure) {
+                sb.append("\n");
+                sb.append(((Structure) element).toIndentedString(indent + 1));
+            } else {
+                sb.append(element.toString());
+            }
+
+            if (i < elements.size() - 1) {
+                sb.append(" ");
+            }
+        }
+
+        sb.append(")");
+        return sb.toString();
+    }
+
     public List<Element> getElements() {
         return elements;
     }
