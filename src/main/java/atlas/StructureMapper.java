ab0f05cecb68e54b4a866e97462b6c3514802feb
Felim Sullivan <felim.sullivan@gmail.com>
2026-03-30 16:15:01 +0100

    added-sprint-2-structure-and-function-stubs

---
+++ src/main/java/atlas/StructureMapper.java
@@ -0,0 +1,16 @@
+package atlas;
+
+import java.util.Map;
+
+public class StructureMapper {
+
+    public static Boolean isMappable(Structure a, Structure b){
+        // TODO - User Story 2.1 (part 1)
+        return false;
+    }
+
+    public static Map<String, String> generateMapping(Structure a, Structure b){
+        // TODO - User Story 2.1 (part 2)
+        return null;
+    }
+}
