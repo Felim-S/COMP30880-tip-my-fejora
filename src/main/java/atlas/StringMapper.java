e82baf34d5a2c81fde9afc4d49afe349e54caf48
Felim Sullivan <felim.sullivan@gmail.com>
2026-03-30 16:15:01 +0100

    added-sprint-2-structure-and-function-stubs

---
+++ src/main/java/atlas/StringMapper.java
@@ -0,0 +1,10 @@
+package atlas;
+
+import java.util.Map;
+
+public class StringMapper {
+    public static Map<String, String> generateMapping(String a, String b){
+        // TODO - User Story 2.2
+        return null;
+    }
+}
