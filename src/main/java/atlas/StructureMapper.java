8f203c9d6b494c1ceacdf8492e43fa1cacd2b49f
7joey <joeymcentee321@gmail.com>
2026-03-31 16:38:55 +0100

    isMappable improvement

--- src/main/java/atlas/StructureMapper.java
+++ src/main/java/atlas/StructureMapper.java
@@ -7,67 +7,11 @@ import java.util.Map;
 public class StructureMapper {
 
     public static Boolean isMappable(Structure A, Structure B){
-        Map<String, String> map = new HashMap<>();
-        return checkMapping(A, B, map);
+        String a = StructureAbstractor.generateAbstraction(A).toString();
+        String b = StructureAbstractor.generateAbstraction(B).toString();
+        return a.equals(b);
     }
 
-    private static boolean checkMapping(Structure A, Structure B, Map<String, String> map) {
-        List<Element> elementsA = A.getElements();
-        List<Element> elementsB = B.getElements();
-
-        if(elementsA.size() != elementsB.size()){
-            return false;
-        }
-
-        for(int i = 0; i < elementsA.size(); i++){
-            Element elementA = elementsA.get(i);
-            Element elementB = elementsB.get(i);
-
-            if(elementA instanceof Structure && elementB instanceof Structure){
-                if(!checkMapping((Structure) elementA, (Structure) elementB, map)){
-                    return false;
-                }
-                continue;
-            }
-
-            //if one is a structure and other one isnt then shapes dont match
-            if(elementA instanceof Structure || elementB instanceof Structure){
-                return false;
-            }
-
-            //predicates must match exactly
-            if (elementA instanceof Predicate && elementB instanceof Predicate){
-                if(!elementA.toString().equals(elementB.toString())){
-                    return false;
-                }
-                continue;
-            }
-
-            if(elementA instanceof Predicate || elementB instanceof Predicate){
-                return false;
-            }
-
-            //both are symbols, check asterisk rule and 1-1 consistency
-            String valA =  elementA.toString();
-            String valB = elementB.toString();
-
-            if(valA.startsWith("*") != valB.startsWith("*")){
-                return false;
-            }
-
-            if(map.containsKey(valA)){
-                if(!map.get(valA).equals(valB)){
-                    return false;
-                }
-            } else {
-                if(map.containsValue(valB)){
-                    return false;
-                }
-                map.put(valA, valB);
-            }
-        }
-        return true;
-    }
 
     public static Map<String, String> generateMapping(Structure A, Structure B){
         if(!isMappable(A,B)){ return null; }
