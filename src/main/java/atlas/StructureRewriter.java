b1e1e64a60ad2565eb99ee83e95b504a28f9d51c
Felim Sullivan <felim.sullivan@gmail.com>
2026-04-07 16:33:56 +0100

    defined-class-structure-idea-for-sprint-3

---
+++ src/main/java/atlas/StructureRewriter.java
@@ -0,0 +1,18 @@
+package atlas;
+
+import java.util.List;
+import java.util.Map;
+
+public class StructureRewriter {
+
+    private final Map<String, List<Rule>> rules;
+
+    public StructureRewriter(Map<String, List<Rule>> rules) {
+        this.rules = rules;
+    }
+
+    public List<Structure> rewrite(Structure structure) {
+        // TODO : User story 3.2
+        return null;
+    }
+}
