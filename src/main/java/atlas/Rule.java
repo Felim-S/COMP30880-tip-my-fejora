7797506627531bb46956c449b7cbb3521ab68445
Felim Sullivan <felim.sullivan@gmail.com>
2026-04-07 16:33:56 +0100

    defined-class-structure-idea-for-sprint-3

---
+++ src/main/java/atlas/Rule.java
@@ -0,0 +1,5 @@
+package atlas;
+
+public class Rule {
+    // TODO : User story 3.1
+}
---
+++ src/main/java/atlas/RuleParser.java
@@ -0,0 +1,11 @@
+package atlas;
+
+import java.util.List;
+import java.util.Map;
+
+public class RuleParser {
+    public static Map<String, List<Rule>> parse(String filename){
+        // TODO : User story 3.1
+        return null;
+    }
+}
